38f9c705259d7c59b701e65f8e6bda2fc22c4a3a
garg-lucifer <naman33jii@gmail.com>
2021-07-03 22:24:55 +0530

    Commit

--- app/src/main/java/com/doodat/gfgfadingactionbar/MainActivity.java
+++ app/src/main/java/com/doodat/gfgfadingactionbar/MainActivity.java
@@ -14,9 +14,12 @@ public class MainActivity extends Activity {  // extend the MainActivity to Acti
         // Adding FadingActionBar in the Activity
 
         FadingActionBarHelper helper = new FadingActionBarHelper()
-                .actionBarBackground(R.drawable.ab_background) // background of actionBar in collapsed form
-                .headerLayout(R.layout.header) // header layout of actionBar in expanded form
+                .actionBarBackground(R.drawable.ab_background)
+                // actionBarBackground sets the background of actionBar in collapsed form
+                .headerLayout(R.layout.header)
+                // headerLayout sets the header layout of actionBar in expanded form
                 .contentLayout(R.layout.activity_main);
+                // contentLayout sets the main content below action bar
         setContentView(helper.createView(this));
         helper.initActionBar(this);
 
